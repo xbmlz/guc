44a8cfe4d1579ff0ffccd7c27f8bf195100467a0
xbmlz <997909544@qq.com>
2024-01-12 14:39:17 +0800

    feat: add ustring

--- ustring/ustring.go
+++ ustring/ustring.go
@@ -1,6 +1,10 @@
 package ustring
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
 
 // IsEmpty returns true if the string is empty.
 func IsEmpty(s string) bool {
@@ -20,4 +24,96 @@ func IsNotEmpty(s string) bool {
 // IsNotBlank returns true if the string is not empty and contains at least one non-white space codepoint.
 func IsNotBlank(s string) bool {
 	return !IsBlank(s)
+}
+
+// HasEmpty returns true if any of the strings is empty.
+func HasEmpty(s ...string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for _, v := range s {
+		if IsEmpty(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// HasBlank returns true if any of the strings is empty or contains only white space codepoints.
+func HasBlank(s ...string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for _, v := range s {
+		if IsBlank(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// RemovePrefix removes the prefix from the string.
+// e.g. RemovePrefix("abc", "a") => "bc"
+func RemovePrefix(s, p string) string {
+	if IsEmpty(s) || IsEmpty(p) {
+		return s
+	}
+	if strings.HasPrefix(s, p) {
+		return s[len(p):]
+	}
+	return s
+}
+
+// RemoveSuffix removes the suffix from the string.
+// e.g. RemoveSuffix("abc", "c") => "ab"
+func RemoveSuffix(s, p string) string {
+	if IsEmpty(s) || IsEmpty(p) {
+		return s
+	}
+	if strings.HasSuffix(s, p) {
+		return s[:len(s)-len(p)]
+	}
+	return s
+}
+
+// Format replaces "{}" with "%v" in the string.
+// e.g. Format("abc{}def", "123") => "abc123def"
+func Format(s string, args ...interface{}) string {
+	res := s
+	for _, arg := range args {
+		res = strings.Replace(res, "{}", fmt.Sprint(arg), 1)
+	}
+	return res
+}
+
+// Capitalize returns a copy of the string with its first character capitalized and the rest lowercased.
+func Capitalize(s string) string {
+	if IsEmpty(s) {
+		return s
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+// Chomp Removes one newline from end of a String if it's there, otherwise leave it alone. A newline is "\n", "\r", or "\r\n".
+func Chomp(s string) string {
+	for _, suffix := range []string{"\r\n", "\n", "\r"} {
+		if strings.HasSuffix(s, suffix) {
+			return strings.TrimSuffix(s, suffix)
+		}
+	}
+	return s
+}
+
+// Chop Remove the last character from a String.
+func Chop(s string) string {
+	if IsEmpty(s) {
+		return s
+	}
+	_, size := utf8.DecodeLastRuneInString(s)
+	if strings.HasSuffix(s, "\r\n") {
+		return s[:len(s)-2]
+	} else if strings.HasSuffix(s, "\n") || strings.HasSuffix(s, "\r") {
+		return s[:len(s)-1]
+	}
+	return s[:len(s)-size]
 }
--- ustring/ustring_test.go
+++ ustring/ustring_test.go
@@ -41,3 +41,185 @@ func TestIsBlank(t *testing.T) {
 		})
 	}
 }
+
+func TestHasEmpty(t *testing.T) {
+	tests := []struct {
+		name string
+		s    []string
+		want bool
+	}{
+		{"not empty and empty", []string{"abc", ""}, true},
+		{"not empty", []string{"abc", "123"}, false},
+		{"empty and tab", []string{" ", "\t", "\n"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HasEmpty(tt.s...); got != tt.want {
+				t.Errorf("HasEmpty(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHasBlank(t *testing.T) {
+	tests := []struct {
+		name string
+		s    []string
+		want bool
+	}{
+		{"not empty and empty", []string{"abc", ""}, true},
+		{"not empty", []string{"abc", "123"}, false},
+		{"empty and tab", []string{" ", "\t", "\n"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HasBlank(tt.s...); got != tt.want {
+				t.Errorf("HasBlank(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRemovePrefix(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		p    string
+		want string
+	}{
+		{"empty", "", "", ""},
+		{"empty prefix", "abc", "", "abc"},
+		{"empty string", "", "abc", ""},
+		{"prefix not match", "abc", "xyz", "abc"},
+		{"prefix match", "abc", "ab", "c"},
+		{"prefix match", "abc", "c", "abc"},
+		{"prefix match", "1.jpg", "1.", "jpg"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemovePrefix(tt.s, tt.p); got != tt.want {
+				t.Errorf("RemovePrefix(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRemoveSuffix(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		p    string
+		want string
+	}{
+		{"empty", "", "", ""},
+		{"empty suffix", "abc", "", "abc"},
+		{"empty string", "", "abc", ""},
+		{"suffix not match", "abc", "xyz", "abc"},
+		{"suffix match", "abc", "bc", "a"},
+		{"suffix match", "abc", "a", "abc"},
+		{"suffix match", "1.jpg", ".jpg", "1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemoveSuffix(tt.s, tt.p); got != tt.want {
+				t.Errorf("RemoveSuffix(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormat(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		args []interface{}
+		want string
+	}{
+		{"empty", "", nil, ""},
+		{"empty args", "abc", nil, "abc"},
+		{"args not match", "abc", []interface{}{"xyz"}, "abc"},
+		{"args match", "abc{}", []interface{}{"d"}, "abcd"},
+		{"args match", "abc{}{}fg", []interface{}{"d", "e"}, "abcdefg"},
+		{"args match", "abc{}123{}ABC{}", []interface{}{"d", "4", "D"}, "abcd1234ABCD"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Format(tt.s, tt.args...); got != tt.want {
+				t.Errorf("Format(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCapitalize(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{"empty", "", ""},
+		{"lower case", "cat", "Cat"},
+		{"upper case", "cAt", "CAt"},
+		{"mixed case", "'cat'", "'cat'"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Capitalize(tt.s); got != tt.want {
+				t.Errorf("Capitalize(): %v = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChomp(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{"empty", "", ""},
+		{"new line", "abc \r", "abc "},
+		{"new line", "abc\n", "abc"},
+		{"new line", "abc\r\n", "abc"},
+		{"new line", "abc\r\n\r\n", "abc\r\n"},
+		{"new line", "abc\n\r", "abc\n"},
+		{"new line", "你好\n\r", "你好\n"},
+		{"new line", "\r", ""},
+		{"new line", "\n", ""},
+		{"new line", "\r\n", ""},
+	}
+	for ti, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Chomp(tt.s); got != tt.want {
+				t.Errorf("Chomp(): %v-%d = %v, want %v", tt.name, ti, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChop(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{"empty", "", ""},
+		{"new line", "abc \r", "abc "},
+		{"new line", "abc\n", "abc"},
+		{"new line", "abc\r\n", "abc"},
+		{"new line", "abc", "ab"},
+		{"new line", "你好", "你"},
+		{"new line", "abc\nabc", "abc\nab"},
+		{"new line", "a", ""},
+		{"new line", "\r", ""},
+		{"new line", "\n", ""},
+		{"new line", "\r\n", ""},
+	}
+	for ti, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Chop(tt.s); got != tt.want {
+				t.Errorf("Chop(): %v-%d = %v, want %v", tt.name, ti, got, tt.want)
+			}
+		})
+	}
+}
